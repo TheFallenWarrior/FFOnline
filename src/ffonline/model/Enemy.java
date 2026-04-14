3bce6895cbb2d4f6fdaaeb2dfa7a34dfa3d53841
TheFallenWarrior <thefallenwarrior666@gmail.com>
2026-04-13 22:12:51 -0300

    Populate class Enemy with fields and implement constructor

--- src/ffonline/model/Enemy.java
+++ src/ffonline/model/Enemy.java
@@ -23,6 +23,11 @@
  */
 package ffonline.model;
 
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.List;
+import java.util.logging.Logger;
+
 /**
  *
  * @author thefa
@@ -31,17 +36,70 @@ public class Enemy extends Battler{
     private static final int MAGIC_MAX_INVENTORY = 8;
     private static final int SKILL_MAX_INVENTORY = 4;
     
-    private int exp;
-    private int gil;
+    private static final Logger LOGGER = Logger.getLogger(Enemy.class.getName());
+    public static final String JSON_PATH = "json/enemy.json";
     
-    private int morale;
-    private int baseHitsPerTurn
+    private final int exp;
+    private final int gil;
+    
+    private final int morale;
+    private final int baseHitsPerTurn;
+    private final EnumSet<EnemyType> enemyTypes;
+    private final EnumSet<StatusAilment> attackStatuses;
     
     private final Inventory<Magic> magicInventory = new Inventory<>(MAGIC_MAX_INVENTORY);
-    private int magicChance;
+    private final int magicChance;
     
     private final Inventory<Magic> skillInventory = new Inventory<>(SKILL_MAX_INVENTORY);
-    private int skillChance;
+    private final int skillChance;
+    
+    public Enemy(
+        String name,
+        int exp,
+        int gil,
+        int hp,
+        int morale,
+        List<Magic> magicInventory,
+        int magicChance,
+        List<Magic> skillInventory,
+        int skillChance,
+        int evadeChance,
+        int absorb,
+        int baseHitsPerTurn,
+        int hitChance,
+        int damage,
+        int critChance,
+        EnumSet<Element> elementalOffense,
+        EnumSet<StatusAilment> attackStatuses,
+        EnumSet<EnemyType> enemyTypes,
+        int magicDefense,
+        EnumSet<Element> elementalWeaknesses,
+        EnumSet<Element> elementalResistances
+    ){
+        super(
+            hp,
+            damage,
+            absorb,
+            hitChance,
+            critChance,
+            evadeChance,
+            magicDefense,
+            elementalOffense,
+            elementalWeaknesses,
+            elementalResistances
+        );
+        setName(name);
+        this.exp = exp&0xffff;
+        this.gil = gil&0xffff;
+        this.morale = morale&0xff;
+        this.magicInventory.addAll(magicInventory);
+        this.magicChance = magicChance&0xff;
+        this.skillInventory.addAll(skillInventory);
+        this.skillChance = skillChance&0xff;
+        this.baseHitsPerTurn = baseHitsPerTurn&0xff;
+        this.attackStatuses = EnumSet.copyOf(attackStatuses);
+        this.enemyTypes = EnumSet.copyOf(enemyTypes);
+    }
     
     @Override
     public boolean canReceiveStatus(StatusAilment status){
@@ -52,12 +110,45 @@ public class Enemy extends Battler{
     public void battleEnd(){
         setHp(0);
     }
+
+    public int getExp() {
+        return exp;
+    }
+
+    public int getGil() {
+        return gil;
+    }
+
+    public int getMorale() {
+        return morale;
+    }
+
+    public EnumSet<EnemyType> getEnemyTypes() {
+        return enemyTypes;
+    }
+
+    public EnumSet<StatusAilment> getAttackStatuses() {
+        return attackStatuses;
+    }
+
+    public List<Magic> getMagicInventory(){
+        return Collections.unmodifiableList(magicInventory);
+    }
     
-    @Override
-    public int getCritChance() {
-        return 0;
+    public int getMagicChance() {
+        return magicChance;
+    }
+    
+    public List<Magic> getSkillInventory(){
+        return Collections.unmodifiableList(skillInventory);
+    }
+
+    public int getSkillChance() {
+        return skillChance;
     }
     
     @Override
-    public int getBaseHitsPerTurn(){ return 1; }
+    public int getBaseHitsPerTurn(){
+        return baseHitsPerTurn;
+    }
 }
