55881b8178cd0ba5e5245a51c43a4c2af202e24f
TheFallenWarrior <thefallenwarrior666@gmail.com>
2026-06-03 07:42:38 -0300

    Fix an off-by-one bug in Presentation.characterStats

--- src/ffonline/controller/Presentation.java
+++ src/ffonline/controller/Presentation.java
@@ -96,7 +96,7 @@ public class Presentation {
     
     public static String characterStats(PlayerCharacter character){
         // INTENTIONAL: Experience displays wrong values for numbers smaller than
-        //  0 or bigger than 999999. However, in such cases, '%999999' won't yield
+        //  0 or bigger than 999999. However, in such cases, '%1000000' won't yield
         //  the same results as FF's 24-bit int to decimal conversion.
         return String.format(
             "%s - %s - LEV %2d\n\n" +
@@ -110,8 +110,8 @@ public class Presentation {
             character.getName(),
             character.getJob().displayName(),
             character.getLevel(),
-            character.getExp()%999999,
-            character.getExpForNextLevel()%999999,
+            character.getExp()%1000000,
+            character.getExpForNextLevel()%1000000,
             character.getStrength(),
             character.getDamage(),
             character.getAgility(),
