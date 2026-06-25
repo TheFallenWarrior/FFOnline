a6247fad6d42cfe61dc81c513493f2776e61e37f
TheFallenWarrior <thefallenwarrior666@gmail.com>
2026-06-25 15:48:33 -0300

    Use a dispatch table for handling spell effects in MagicCommand

--- src/ffonline/model/command/MagicCommand.java
+++ src/ffonline/model/command/MagicCommand.java
@@ -32,7 +32,10 @@ import ffonline.model.EnemyType;
 import ffonline.model.Magic;
 import ffonline.model.PlayerCharacter;
 import ffonline.model.StatusAilment;
+import java.util.EnumMap;
 import java.util.EnumSet;
+import java.util.Map;
+import java.util.function.Consumer;
 
 /**
  *
@@ -41,6 +44,7 @@ import java.util.EnumSet;
 public class MagicCommand extends BattleCommand {
     private final Magic spell;
     private final CommandResult.Builder builder = CommandResult.builder(this);
+    private final Map<Magic.Effect, Consumer<Battler>> effectHandlers = new EnumMap(Magic.Effect.class);
     
     public MagicCommand(
         BattlerGroup<? extends Battler> allies,
@@ -52,6 +56,33 @@ public class MagicCommand extends BattleCommand {
     ){
         super(allies, enemies, actor, allyTarget, enemyTarget, spell.getTargeting());
         this.spell = spell;
+        registerEffectHandlers();
+    }
+    
+    private void registerEffectHandlers(){
+        effectHandlers.put(Magic.Effect.NOTHING, ((t) -> {
+            builder.ineffective(t);
+        }));
+        effectHandlers.put(Magic.Effect.DAMAGE, this::applyDamage);
+        effectHandlers.put(Magic.Effect.HARM, this::applyHarm);
+        effectHandlers.put(Magic.Effect.HIT_MULTIPLIER_DOWN, this::applyHitMultiplierDown);
+        effectHandlers.put(Magic.Effect.MORALE_DOWN, this::applyMoraleDown);
+        effectHandlers.put(Magic.Effect.UNUSED, this::applyHpRecovery); 
+        effectHandlers.put(Magic.Effect.HP_RECOVERY, this::applyHpRecovery);
+        effectHandlers.put(Magic.Effect.STATUS_RECOVERY, this::applyStatusRecovery);
+        effectHandlers.put(Magic.Effect.DEFENSE_UP, this::applyDefenseUp);
+        effectHandlers.put(Magic.Effect.RESIST_ELEMENT, this::applyResistElement);
+        effectHandlers.put(Magic.Effect.ATTACK_UP, this::applyAttackUp);
+        effectHandlers.put(Magic.Effect.HIT_MULTIPLIER_UP, this::applyHitMultiplierUp);
+        effectHandlers.put(Magic.Effect.ATTACK_ACCURACY_UP, this::applyAttackAccuracyUp);
+        effectHandlers.put(Magic.Effect.EVASION_DOWN, ((t) -> {
+            // INTENTIONAL: Evasion down always misses
+            builder.fail(t);
+        }));
+        effectHandlers.put(Magic.Effect.FULL_RECOVERY, this::applyFullRecovery);
+        effectHandlers.put(Magic.Effect.EVASION_UP, this::applyEvasionUp);
+        effectHandlers.put(Magic.Effect.UNRESIST_ELEMENT, this::applyUnresistElement);
+        effectHandlers.put(Magic.Effect.HP300_STATUS, this::applyHp300Status);
     }
     
     /**
@@ -89,157 +120,8 @@ public class MagicCommand extends BattleCommand {
     }
     
     private void apply(Battler target){
-        switch(spell.getEffect()){
-            case NOTHING -> { builder.ineffective(target); }
-            
-            case DAMAGE -> {
-                int effectivity = spell.getEffectivity();
-                
-                if(enumSetContainsAny(target.getElementalResistances(), spell.getElements()))
-                    effectivity -= effectivity/2;
-                if(enumSetContainsAny(target.getElementalWeaknesses(), spell.getElements()))
-                    effectivity += effectivity/2;
-                
-                int damage = calculateDamage(effectivity);
-                if(calculateHit(target)) damage *= 2;
-
-                target.offsetHp(-damage);
-                builder.hit(target, damage);
-            }
-            
-            case HARM -> {
-                if(!(target instanceof Enemy enemy) || !enemy.getEnemyTypes().contains(EnemyType.UNDEAD)){
-                    builder.ineffective(target);
-                    break;
-                }
-                
-                int damage = calculateDamage();
-                if(calculateHit(target)) damage *= 2;
-
-                target.offsetHp(-damage);
-                builder.hit(target, damage);
-            }
-            
-            case HIT_MULTIPLIER_DOWN -> {
-                if(calculateHit(target)){
-                    target.increaseHitMultiplier();
-                    builder.succeed(target);
-                } else
-                    builder.fail(target);
-            }
-            
-            case MORALE_DOWN -> {
-                if(target instanceof Enemy enemy){
-                    enemy.offsetMorale(-spell.getEffectivity());
-                } else
-                    builder.ineffective(target);
-            }
-            
-            // INTENTIONAL: "Unused" spell effect behaves like healing
-            case UNUSED, HP_RECOVERY -> {
-                int recovery = calculateDamage();
-                target.offsetHp(recovery);
-                builder.hit(target, recovery);
-            }
-            
-            case STATUS_RECOVERY -> {
-                EnumSet<StatusAilment> toRemove = spell.getEffectStatuses();
-                toRemove.removeAll(EnumSet.of(StatusAilment.DEAD, StatusAilment.PETRIFIED));
-                target.removeAllStatuses(toRemove);
-                
-                if(target.isAlive())
-                    builder.succeed(target);
-                else
-                    builder.ineffective(target);
-            }
-            
-            case DEFENSE_UP -> {
-                int absorb = target.getAbsorb() + spell.getEffectivity();
-                target.setAbsorb(absorb);
-            }
-            
-            case RESIST_ELEMENT -> {
-                EnumSet<Element> resistances = target.getElementalResistances();
-                resistances.addAll(spell.getEffectElements());
-                target.setElementalResistances(resistances);
-                
-                builder.succeed(target);
-            }
-            
-            case ATTACK_UP -> {
-                // INTENTIONAL: "Attack up" spells do work on player characters
-                if(target instanceof PlayerCharacter pc){
-                    builder.ineffective(pc);
-                    break;
-                }
-                @SuppressWarnings("null")
-                int damage = target.getDamage() + spell.getEffectivity();
-                target.setDamage(damage);
-                builder.succeed(target);
-            }
-            
-            case HIT_MULTIPLIER_UP -> {
-                target.increaseHitMultiplier();
-                builder.succeed(target);
-            }
-            
-            case ATTACK_ACCURACY_UP -> {
-                // INTENTIONAL: "Attack/accuracy up" spells do not work on player characters
-                if(target instanceof PlayerCharacter pc){
-                    builder.ineffective(pc);
-                    break;
-                }
-                @SuppressWarnings("null")
-                int damage = target.getDamage() + spell.getEffectivity();
-                target.setDamage(damage);
-                // INTENTIONAL: The spell's accuracy stat is added to the target's accuracy
-                int hitChance = target.getHitChance() + spell.getAccuracy();
-                target.setHitChance(hitChance);
-                builder.succeed(target);
-            }
-            
-            case EVASION_DOWN -> {
-                // INTENTIONAL: Evasion down always misses
-                builder.fail(target);
-            }
-            
-            case FULL_RECOVERY -> {
-                target.setHp(target.getMaxHp());
-                
-                // Remove all statuses except death and stone
-                EnumSet<StatusAilment> toRemove = EnumSet.allOf(StatusAilment.class);
-                toRemove.removeAll(EnumSet.of(StatusAilment.DEAD, StatusAilment.PETRIFIED));
-                target.removeAllStatuses(toRemove);
-                
-                builder.succeed(target);
-            }
-            
-            case EVASION_UP -> {
-                int evadeChance = target.getEvadeChance() + spell.getEffectivity();
-                target.setEvadeChance(evadeChance);
-                builder.succeed(target);
-            }
-            
-            case UNRESIST_ELEMENT -> {
-                // INTENTIONAL: "Remove resistance" spell effect does not work on enemies
-                if(target instanceof PlayerCharacter character && calculateHit(character)){
-                    character.setElementalResistances(EnumSet.noneOf(Element.class));
-                    builder.succeed(target);
-                } else
-                    builder.fail(target);
-            }
-            
-            case HP300_STATUS -> {
-                if(
-                    target.getHp() < 300 &&
-                    !enumSetContainsAny(target.getElementalResistances(), spell.getEffectElements())
-                ){
-                    target.addAllStatuses(spell.getEffectStatuses());
-                    builder.succeed(target);
-                } else
-                    builder.fail(target);
-            }
-        }        
+        Consumer<Battler> handler = effectHandlers.get(spell.getEffect());
+        handler.accept(target);    
     }
     
     @Override
@@ -274,5 +156,148 @@ public class MagicCommand extends BattleCommand {
         }
         
         return builder.build();
+    }
+    
+    private void applyDamage(Battler target){
+        int effectivity = spell.getEffectivity();
+                
+        if(enumSetContainsAny(target.getElementalResistances(), spell.getElements()))
+            effectivity -= effectivity/2;
+        if(enumSetContainsAny(target.getElementalWeaknesses(), spell.getElements()))
+            effectivity += effectivity/2;
+
+        int damage = calculateDamage(effectivity);
+        if(calculateHit(target)) damage *= 2;
+
+        target.offsetHp(-damage);
+        builder.hit(target, damage);
+    }
+    
+    private void applyHarm(Battler target){
+        if(!(target instanceof Enemy enemy) || !enemy.getEnemyTypes().contains(EnemyType.UNDEAD)){
+            builder.ineffective(target);
+            return;
+        }
+
+        int damage = calculateDamage();
+        if(calculateHit(target)) damage *= 2;
+
+        target.offsetHp(-damage);
+        builder.hit(target, damage);
+    }
+    
+    private void applyHitMultiplierDown(Battler target){
+        if(calculateHit(target)){
+            target.increaseHitMultiplier();
+            builder.succeed(target);
+        } else
+            builder.fail(target);
+    }
+    
+    private void applyMoraleDown(Battler target){
+        if(target instanceof Enemy enemy){
+            enemy.offsetMorale(-spell.getEffectivity());
+        } else
+            builder.ineffective(target);
+    }
+    
+    private void applyHpRecovery(Battler target){
+        int recovery = calculateDamage();
+        target.offsetHp(recovery);
+        builder.hit(target, recovery);
+    }
+    
+    private void applyStatusRecovery(Battler target){
+        EnumSet<StatusAilment> toRemove = spell.getEffectStatuses();
+        toRemove.removeAll(EnumSet.of(StatusAilment.DEAD, StatusAilment.PETRIFIED));
+        target.removeAllStatuses(toRemove);
+
+        if(target.isAlive())
+            builder.succeed(target);
+        else
+            builder.ineffective(target);
+    }
+    
+    private void applyDefenseUp(Battler target){
+        int absorb = target.getAbsorb() + spell.getEffectivity();
+        target.setAbsorb(absorb);
+        builder.succeed(target);
+    }
+    
+    private void applyResistElement(Battler target){
+        EnumSet<Element> resistances = target.getElementalResistances();
+        resistances.addAll(spell.getEffectElements());
+        target.setElementalResistances(resistances);
+
+        builder.succeed(target);
+    }
+    
+    private void applyAttackUp(Battler target){
+        // INTENTIONAL: "Attack up" spells do work on player characters
+        if(target instanceof PlayerCharacter pc){
+            builder.ineffective(pc);
+            return;
+        }
+        @SuppressWarnings("null")
+        int damage = target.getDamage() + spell.getEffectivity();
+        target.setDamage(damage);
+        builder.succeed(target);
+    }
+    
+    private void applyHitMultiplierUp(Battler target){
+        target.increaseHitMultiplier();
+        builder.succeed(target);
+    }
+    
+    private void applyAttackAccuracyUp(Battler target){
+        // INTENTIONAL: "Attack/accuracy up" spells do not work on player characters
+        if(target instanceof PlayerCharacter pc){
+            builder.ineffective(pc);
+            return;
+        }
+        @SuppressWarnings("null")
+        int damage = target.getDamage() + spell.getEffectivity();
+        target.setDamage(damage);
+        // INTENTIONAL: The spell's accuracy stat is added to the target's accuracy
+        int hitChance = target.getHitChance() + spell.getAccuracy();
+        target.setHitChance(hitChance);
+        builder.succeed(target);
+    }
+    
+    private void applyFullRecovery(Battler target){
+        target.setHp(target.getMaxHp());
+                
+        // Remove all statuses except death and stone
+        EnumSet<StatusAilment> toRemove = EnumSet.allOf(StatusAilment.class);
+        toRemove.removeAll(EnumSet.of(StatusAilment.DEAD, StatusAilment.PETRIFIED));
+        target.removeAllStatuses(toRemove);
+
+        builder.succeed(target);
+    }
+    
+    private void applyEvasionUp(Battler target){
+        int evadeChance = target.getEvadeChance() + spell.getEffectivity();
+        target.setEvadeChance(evadeChance);
+        builder.succeed(target);
+    }
+    
+    private void applyUnresistElement(Battler target){
+        // INTENTIONAL: "Remove resistance" spell effect does not work on enemies
+        if(target instanceof PlayerCharacter character && calculateHit(character)){
+            character.setElementalResistances(EnumSet.noneOf(Element.class));
+            builder.succeed(target);
+        } else
+            builder.fail(target);
+    }
+    
+    private void applyHp300Status(Battler target){
+        if(
+            target.getHp() < 300 &&
+            !enumSetContainsAny(target.getElementalResistances(), spell.getEffectElements())
+        ){
+            target.addAllStatuses(spell.getEffectStatuses());
+            builder.succeed(target);
+        } else
+            builder.fail(target);
     }
 }
